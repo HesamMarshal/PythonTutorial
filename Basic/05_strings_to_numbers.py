e7664568971c9da259d1645c2db0feff8c2d79da
Hesam Marshal <heak82@hotmail.com>
2021-09-18 15:17:53 +0430

    string to numbers file

---
+++ Basic/05_strings_to_numbers.py
@@ -0,0 +1,25 @@
+# x = 2
+# y = 3
+# sum = x + y
+# print(sum)
+
+# k = "2"
+# z = "3"
+# result = k + z
+# print(result)  # "2" + "3" => "23"
+
+# value1 = int(k)
+# value2 = int(z)
+# sum2 = value1 + value2
+# print(sum2)
+
+x = "6.5"
+y = "8.35"
+z = float(x)
+k = float(y)
+result = z + k
+print(result)
+result = float(x) + float(y)
+print(result)
+
+print(float(x)+float(y))
