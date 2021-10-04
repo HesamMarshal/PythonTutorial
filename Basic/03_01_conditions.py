093dd9e2395ff321d7411e8cd27c5133c4c86ed0
Hesam Marshal <heak82@hotmail.com>
2021-10-04 14:34:04 +0330

    Conditions Added

---
+++ Basic/03_01_conditions.py
@@ -0,0 +1,22 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : HesamMarshal.ir              #
+# instagram: @HesamMarshal               #
+##########################################
+
+print(F"6 > 3 : {6 > 3}")
+print(F"6 > 7 : {6 > 7}")
+print(F"6 < 20 : {6 < 20}")
+print(F"6 < 3 : {6 < 3}")
+print(F"6 == 3 : {6 == 3}")
+print(F"6 == 6 : {6 == 6}")
+
+print(F"6 >= 6 : {6 >= 6}")
+print(F"6 > 6 : {6 > 6}")
+
+print(F"6 != 6 : {6 != 6}")
+print(F"6 != 7 : {6 != 7}")
+
+print(10 == "10")
+
+print("hesam" > "marshal")
