18577e6ff44e891aee84fe6fe9ab9a727883f53e
Hesam Marshal <heak82@hotmail.com>
2021-10-15 12:00:39 +0330

    lesson 03_02_if.py added

---
+++ Basic/03_02_if.py
@@ -0,0 +1,12 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : HesamMarshal.ir              #
+# instagram: @HesamMarshal               #
+##########################################
+
+temperature = 35
+
+if temperature > 30:
+    print("It's warm there!")
+    print("Drink water!")
+print("Done")
