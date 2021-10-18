4bd40636e3ae374c24e9889d327207ecb68f6a2f
Hesam Marshal <heak82@hotmail.com>
2021-10-18 19:09:34 +0330

    Add a few lines of comments

--- Basic/03_03_if_if.py
+++ Basic/03_03_if_if.py
@@ -1,6 +1,12 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : HesamMarshal.ir              #
+# instagram: @HesamMarshal               #
+##########################################
+
 score = int(input("Enter The Score: "))
 
-if score < 10 :
+if score < 10:
     print("Fail")
 
 if score >= 10:
