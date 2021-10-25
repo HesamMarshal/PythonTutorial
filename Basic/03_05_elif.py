3fcc4c09a1712cf2d1280ece1bfbe395ee2d117b
Hesam Marshal <heak82@hotmail.com>
2021-10-25 12:05:40 +0330

    ELSE and ELIF

---
+++ Basic/03_05_elif.py
@@ -0,0 +1,16 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : HesamMarshal.ir              #
+# instagram: @HesamMarshal               #
+##########################################
+
+score = 7
+
+if score >= 17:
+    print("A")
+elif score >= 14:
+    print("B")
+elif score >= 10:
+    print("C")
+else:
+    print("F")
