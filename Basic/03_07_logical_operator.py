bc644a4873fec860dd8010913057c74e0f644c72
Hesam Marshal <heak82@hotmail.com>
2021-11-07 10:21:20 +0330

    Logical Operator and

---
+++ Basic/03_07_logical_operator.py
@@ -0,0 +1,11 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : HesamMarshal.ir              #
+# instagram: @HesamMarshal               #
+##########################################
+
+tempereture = 10
+cloudy = True
+
+if tempereture < 15 and cloudy:
+    print("Take your Umbrella")
