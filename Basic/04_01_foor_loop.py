4d5af9fc43d68b10149ffd626cd8691874e6c7e6
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-03-19 10:55:40 +0330

    add for loop and nested loop

---
+++ Basic/04_01_foor_loop.py
@@ -0,0 +1,9 @@
+
+for i in range(10):
+    print(i+1, " hi")
+
+for i in range(1, 11, 3):
+    print(i)
+
+for i in range(1, 6):
+    print(i * "*")
---
+++ Basic/04_02_nested_loop.py
@@ -0,0 +1,5 @@
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        for k in range(1,11):
+            print(f'{i} * {j} * {k}= {i*j*k}')
