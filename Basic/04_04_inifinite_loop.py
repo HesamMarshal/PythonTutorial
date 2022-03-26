13cdb17f3d681edb098eab9f84c3c5f32cd6823e
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-03-26 12:48:28 +0430

    add inifinite loop file

---
+++ Basic/04_04_inifinite_loop.py
@@ -0,0 +1,19 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : Chaptera.ir                  #
+# instagram: @HesamMarshal               #
+# Infinite Loop                          #
+##########################################
+
+
+# i = 1
+# while i < 6:
+#     print(i * "*")
+#     i += 1
+
+
+number = int(input("Enter the number: "))
+
+while number > 0:
+    print(number * number)
+    number = int(input("Enter the number: "))
