c527caa256076685fd4f8e47d39114b2893aa9aa
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-03-26 12:47:24 +0430

    add while file

---
+++ Basic/04_03_while.py
@@ -0,0 +1,21 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : Chaptera.ir                  #
+# instagram: @HesamMarshal               #
+# While loop                            #
+##########################################
+
+for i in range(1, 10, 3):
+    print(i * "*")
+
+
+i = 1
+while i < 6:
+    print(i * "*")
+    i = i+1
+
+
+i = 1000
+while i > 0:
+    print(i)
+    i //= 2
