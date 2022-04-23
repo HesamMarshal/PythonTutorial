fe1def3e42a2c800093d5a83e5c574cd955dab1e
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-04-23 10:19:11 +0430

    add functions

---
+++ Basic/05_01_functions.py
@@ -0,0 +1,19 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : Chaptera.ir                  #
+# instagram: @HesamMarshal               #
+# Functions                              #
+##########################################
+
+
+def hello():
+    print("Hello, Hesam!")
+    print("Have a good day.")
+    print()
+
+
+# hello()
+# hello()
+# hello()
+for i in range(4):
+    hello()
