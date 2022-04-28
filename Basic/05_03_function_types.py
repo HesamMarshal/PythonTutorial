cedb06e85a07f416952a977d918ba7755a8be0a3
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-04-28 20:42:42 +0430

    add function types

---
+++ Basic/05_03_function_types.py
@@ -0,0 +1,20 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : Chaptera.ir                  #
+# instagram: @HesamMarshal               #
+# Function Types (Task, Return)          #
+##########################################
+
+
+def hello(first_name, last_name):
+    print(f"Hello, {first_name} {last_name}!")
+
+
+def get_hello(first_name, last_name):
+    return f"Hello, {first_name} {last_name}!"
+
+
+# hello("Hesam", "Akrami")
+my_string = get_hello("Hesam", "Akrami")
+
+print(my_string[6:])
