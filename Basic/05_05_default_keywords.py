2793303532c83d0e9f369a8f4520bb6e5a187dc7
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-05-04 10:15:04 +0430

    add default keywords

---
+++ Basic/05_05_default_keywords.py
@@ -0,0 +1,20 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : Chaptera.ir                  #
+# instagram: @HesamMarshal               #
+# Default Value for Keywords             #
+##########################################
+
+
+# # Error:
+# def calculate_area(width,  height=0, lenght,):
+#     area = width * height
+#     print(F"Area = {area}")
+
+def calculate_area(width, height=0):
+    area = width * height
+    print(F"Area = {area}")
+
+
+calculate_area(8, 3)
+calculate_area(9)
