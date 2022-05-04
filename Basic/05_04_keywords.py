d525d143e80d60befc5c0afba3b7d8e046be9578
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-05-04 10:14:35 +0430

    add function keywords

---
+++ Basic/05_04_keywords.py
@@ -0,0 +1,20 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : Chaptera.ir                  #
+# instagram: @HesamMarshal               #
+# Keywords                               #
+##########################################
+
+
+def calculate_area(width, height):
+    area = width * height
+    print(F"Area = {area}")
+
+
+width = int(input("Please enter the width: "))
+height = int(input("Please enter the height: "))
+
+
+calculate_area(width, height)
+calculate_area(8, 9)
+calculate_area(width=8, height=7)
