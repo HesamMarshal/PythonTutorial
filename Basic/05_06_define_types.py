98fc2cc11d57ebcb223521f825c5f9b26c93b1c8
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-05-20 11:28:51 +0430

    add comment to the define types

--- Basic/05_06_define_types.py
+++ Basic/05_06_define_types.py
@@ -1,6 +1,13 @@
-def calculate_area(width: int, height=0)->int:
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : Chaptera.ir                  #
+# instagram: @HesamMarshal               #
+# Define types for function parameters   #
+##########################################
+
+def calculate_area(width: int, height=0) -> int:
     area = width * height
     return area
 
 
-my_area= calculate_area()
+my_area = calculate_area()
