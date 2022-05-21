fd49aee882339133a5ec1d93420733157cee8dfb
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-05-21 21:15:35 +0430

    add lists

---
+++ Basic/06_01_lists.py
@@ -0,0 +1,17 @@
+chars = ['a', 'b', 'c', 'x']
+nums = [1, 2, 3, 4]
+nums2 = [5, 6, 7, 8]
+# print(chars)
+# print(type(chars))
+# print(nums2)
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8]]
+# print(matrix)
+ones = [1] * 5
+# print(ones)
+combined = chars + ones
+# print(combined)
+nums = list(range(1, 11))
+print(nums)
+chars = list("Hesam Akrami")
+
+print(chars)
