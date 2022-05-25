31b17e76be70945e2881e74fcac2e50ae7ac9d31
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-05-25 13:38:18 +0430

    add list access file

---
+++ Basic/06_02_list_access.py
@@ -0,0 +1,34 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : Chaptera.ir                  #
+# instagram: @HesamMarshal               #
+# Access list items                      #
+##########################################
+
+
+fruits = ["Apple", "Orange", "Peach", "Strawberry"]
+# print(fruits)
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[3])
+# print(fruits[-1])
+# print(fruits[-2])
+fruits[-1] = "Banana"
+print(fruits)
+
+# slice
+# print(fruits[0:2])
+# print(fruits[:2])
+# print(fruits[1:3])
+# print(fruits[1:])
+# my_fruits = fruits[1:3]
+# print(my_fruits)
+print(fruits[:])
+print(fruits[::3])
+print(fruits[::-1])
+
+nums = list(range(15))
+print(nums)
+print(nums[::2])
+print(nums[::3])
+print(nums[::-1])
