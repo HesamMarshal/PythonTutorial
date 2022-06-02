4fba11353e1b66efc2da64dcb702b71b735f3b09
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-06-02 10:29:51 +0430

    add loop over list file

---
+++ Basic/loop_over_list.py
@@ -0,0 +1,17 @@
+letters = ["a", "b", "c", "d"]
+
+# for letter in letters:
+#     print(letter)
+
+# l = len(letters)
+# # print(l)
+# for i in range(l):
+#     print(i+1, letters[i])
+
+# for letter in enumerate(letters):
+#     # print(letter)
+#     print(letter[0], letter[1])
+
+
+for index, letter in enumerate(letters):
+    print(index+1, letter)
