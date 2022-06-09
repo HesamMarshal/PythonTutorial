0c1ef4d7f222e01747c1bb113a6eeb84b5c68d46
Hesam <14945056+HesamMarshal@users.noreply.github.com>
2022-06-09 11:59:14 +0430

    add find item file

---
+++ Basic/06_06_find_item.py
@@ -0,0 +1,26 @@
+##########################################
+# This code is created by Hesam Marshal  #
+# Website : Chaptera.ir                  #
+# instagram: @HesamMarshal               #
+# List: find an item in the list         #
+##########################################
+todo = ["Breakfast", "Study python", "wash the dishes", "Record a video"]
+letters = ["a", "b", "c", "d"]
+
+position = letters.index("b")
+print(position)
+
+# position = letters.index("d")
+# print(position)
+
+# position = letters.index("x")
+# print(position)
+
+# target = "x"
+
+# if target in letters:
+#     print(target, "is available")
+#     position = letters.index(target)
+#     print(position)
+# else:
+#     print(target, "is Not available")
